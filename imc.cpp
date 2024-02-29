8fac919f7550a7a15c7769973e21485af0367006
Edgard-junio <edgard.pic2019@gmail.com>
2024-02-29 19:45:42 -0300

    tratando erro de variaveis nulas

--- imc.cpp
+++ imc.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
 
-int main(){
-    float altura = 1.5; //definindo o valor da altura
-    float peso = 71.6; //definindo o valor do peso 
-    float Indice = peso/(altura*altura); //calculoda do Indice de massa corporal
+int main() {
+    float altura = 1.5; //definindo o valor da altura em metros
+    float peso = 56; //definindo o valor do peso em kg
 
-    if(Indice < 17) std::cout<<"Muito abaixo do peso";
-    
-    if(Indice >= 17 && Indice < 18.5) std::cout<<"Abaixo do peso";
-    
-    if(Indice >= 18.5 && Indice < 25) std::cout<<"Peso normal";
-    
-    if(Indice >= 25 && Indice < 30) std::cout<<"Acima do peso";
-    
-    if(Indice >= 30 && Indice < 35) std::cout<<"Obesidade";
-    
-    if(Indice >= 35 && Indice < 40) std::cout<<"Obesidade severa";
-    
-    if(Indice >= 40) std::cout<<"Obesidade mórbida";
-    
+    if (altura <= 0 || peso <= 0) {
+        std::cout << "Altura ou peso inválidos";
+    } else {
+        float Indice = peso / (altura * altura); //calculoda do Indice de massa corporal
+
+        if (Indice < 17)
+            std::cout << "Muito abaixo do peso";
+        else if (Indice < 18.5)
+            std::cout << "Abaixo do peso";
+        else if (Indice < 25)
+            std::cout << "Peso normal";
+        else if (Indice < 30)
+            std::cout << "Acima do peso";
+        else if (Indice < 35)
+            std::cout << "Obesidade";
+        else if (Indice < 40)
+            std::cout << "Obesidade severa";
+        else
+            std::cout << "Obesidade mórbida";
+    }
+
     return 0;
 }
